0ff18633abe4f72f37b12624c4aba81888978819
Brijit <bsarkar@stud.hs-offenburg.de>
2022-01-04 13:06:16 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import pms
+from flask import Flask
+
+# pms.passpoli()
+
+print("How many passwords are to be generated")
+n = int(input())
+if n > 1:
+    print('Password generated: ' + pms.ranpassgen(n))
+else:
+    print('Password generated: ' + pms.ranpassgen(n))
