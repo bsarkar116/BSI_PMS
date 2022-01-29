d5f27a3c36e8b0f93d878a3a93dd6d039dcc3286
Brijit <bsarkar@stud.hs-offenburg.de>
2022-01-29 02:45:17 +0100

    Minor change in batch user addition logic.

--- pms.py
+++ pms.py
@@ -63,8 +63,8 @@ def adduser(u, r, appid):
 def batch_adder(file):
     df = pd.read_csv(file)
     templist = []
-    df_new = df.drop(["Role", "AppID"], axis=1)
     if validate_csv(df):
+        df_new = df.drop(["Role", "AppID"], axis=1)
         for i in range(len(df)):
             result, passw = adduser(df.iloc[i, 0], df.iloc[i, 1], df.iloc[i, 2])
             if result:
